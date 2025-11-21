ab73ce7528975f7411b3f7b7c02f969f43627137
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-11-21 15:33:51 -0500

    Add systemd service and timer to automatically upgrade CloudWatch agent
    There are also DEB-specific and RPM-specific shell scripts for
    attempting the upgrade.

---
+++ files/upgrade-cloudwatch-agent-deb.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# Usage:
+#   upgrade-cloudwatch-agent.sh <deb_url>
+#
+# Attempt to upgrade the Amazon CloudWatch Agent.  If the package
+# pointed to is not an update then nothing is changed.
+#
+# The DEB URL must point to a DEB package (*.deb file).
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function usage {
+  cat << HELP
+Usage:
+  ${0##*/} [deb_url]
+
+Attempt to upgrade the Amazon CloudWatch Agent.  If the package
+pointed to is not an update then nothing is changed.
+
+The DEB URL must point to a DEB package (*.deb file).
+HELP
+  exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+else
+  url=${1}
+  pkg_file=/tmp/amazon-cloudwatch-agent.deb
+
+  wget --output-document "$pkg_file" "$url"
+  apt install --assumeyes "$pkg_file"
+fi
---
+++ files/upgrade-cloudwatch-agent-rpm.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Usage:
+#   upgrade-cloudwatch-agent.sh <rpm_url>
+#
+# Attempt to upgrade the Amazon CloudWatch Agent.  If the package
+# pointed to is not an update then nothing is changed.
+#
+# The RPM URL must point to an RPM package (*.rpm file).
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function usage {
+  cat << HELP
+Usage:
+  ${0##*/} [rpm_url]
+
+Attempt to upgrade the Amazon CloudWatch Agent.  If the package
+pointed to is not an update then nothing is changed.
+
+The RPM URL must point to an RPM package (*.rpm file).
+HELP
+  exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+else
+  url=${1}
+
+  dnf5 install --assumeyes "$url"
+fi
